2dd6af17dd43278626730578653dc269f26975d7
BridgerB <git@bridgerb.com>
2022-10-12 22:41:11 -0600

    still working on centering video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,12 +17,26 @@
 	<div>
 		<CommaThree />
 	</div>
+
+	<!-- temp -->
+	<!-- <h1>COMMA THREE</h1>
+  <p>Make driving chill with our beautiful new hardware.</p>
+  <p>But Now ></p>
+  <hr>
+	<hr>
+  <p>Unlike many OEM ADAS systems, openpilot can drive for hours without intervention. We sell a devkit that can run openpilot with 360° HDR vision, compatible with 200+ cars. Starting at $1999. Buy Now</p>
+  <hr>
+  <p>Easy to Install. Watch our step-by-step videos. Install it yourself in as little as 5 minutes.</p>
+  <p>30 Day Money Back Trial. We think you’ll love it, but if you’re not satisfied, you can send it back for a full refund.</p>
+  <p>Pay over time. With Affirm, you can pay monthly for the comma three. Learn more.</p> -->
+
+	
 	<div>
     
-		<TwoEyesOutside />
-		<Openpilot />
-		<Compatibility />
-		<Reviews />
+		<!-- <TwoEyesOutside /> -->
+		<!-- <Openpilot /> -->
+		<!-- <Compatibility /> -->
+		<!-- <Reviews /> -->
 	</div>
 
 </body>
--- src/routes/CommaThree.svelte
+++ src/routes/CommaThree.svelte
@@ -1,50 +1,31 @@
 <body>
-  <!-- <h1>COMMA THREE</h1>
-  <p>Make driving chill with our beautiful new hardware.</p>
-  <p>But Now ></p>
-  <hr> -->
-<div>
-  <img class="phone" src="c3-homepage.png" alt="phone">
   <video class="video" autoplay muted loop>
     <source src="c3screen.mp4" type="video/mp4">
-  </video> 
-</div>
-
-
-  <!-- <hr>
-  <p>Unlike many OEM ADAS systems, openpilot can drive for hours without intervention. We sell a devkit that can run openpilot with 360° HDR vision, compatible with 200+ cars. Starting at $1999. Buy Now</p>
-  <hr>
-  <p>Easy to Install. Watch our step-by-step videos. Install it yourself in as little as 5 minutes.</p>
-  <p>30 Day Money Back Trial. We think you’ll love it, but if you’re not satisfied, you can send it back for a full refund.</p>
-  <p>Pay over time. With Affirm, you can pay monthly for the comma three. Learn more.</p> -->
+  </video>
 </body>
 
 <style>
-  div {
-    border: 100px;
-  }
-
-
-  .phone {
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 100%;
-
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    background: blue;
+    color: white;
+    text-align: center;
+  } 
+  body {
+    background-image: url('c3-homepage.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 80% auto;
+    /* height: 38vh; */
+    height: 310px;
   }
 
   .video {
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 89%;
-    transform: translate(0px, -16%);
+    margin-top: 10px;
+    width: 70%;
+    
   }
+
 
 </style>
